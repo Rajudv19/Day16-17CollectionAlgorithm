583d08df6cc0096ed6df2abf68e6b841ebd7bd7d
Rajudv19 <rajudv1997@gmail.com>
2022-05-18 08:58:24 +0530

    Added code for Binarysearch

---
+++ binarySearch.java
@@ -0,0 +1,38 @@
+package com.BridgeLabs.day16and17;
+
+public class binarySearch {
+
+	static int binarySearch(String[] arr, String x)
+	{
+		int l = 0, r = arr.length - 1;
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			int res = x.compareTo(arr[m]);
+
+			// Check if x is present at mid
+			if (res == 0)
+				return m;
+
+			// If x greater, ignore left half
+			if (res > 0)
+				l = m + 1;
+
+			// If x is smaller, ignore right half
+			else
+				r = m - 1;
+		}
+		return -1;
+	}
+
+	public static void main(String []args) {
+		String[] arr = { "Charliee", "trailer", "is", "extrordinary"};
+		String string = "Charliee";
+		int result = binarySearch(arr, string);
+
+		if (result == -1)
+			System.out.println("Element not present in array --> " + string);
+		else
+			System.out.println("Element found in array --> " + string);
+	}
+}
